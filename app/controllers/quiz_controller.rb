19a986da938951866a8e1f3a21b6c82b36dad126
Jon Glass <jonglass82@gmail.com>
2019-01-16 16:32:10 -0600

    added question_check method and updated all views

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,51 +1,83 @@
 class QuizController < ApplicationController
     
-    $score = 0
-    
+     
     def start
-        
+        $score = 0
     end
     
     
-    def question_1
-        
-        @question1 = Quiz.find(3).question
-        
-    end
     
-    
-    def question_1_check
+    def question_check
         
+        question_number = params[:id]
+        
+        current_question = params[:question]
+        
+        next_question = params[:id].to_i + 1
         
         customer_answer = params[:answer]
         
-        correct_answer = Quiz.find(3).answer
+        correct_answer = Quiz.find(question_number).answer
         
-        if customer_answer = correct_answer
+        if customer_answer == correct_answer
             $score += 1
         else 
             puts "NOT Correct..."
         end
     
         puts $score
-        redirect_to "/question_2"
+        redirect_to "/question_#{next_question}"
+        
+    end
     
+    
+    def question_1
+        @question1 = Quiz.find(1).question
     end
     
    
     def question_2
-        @question2 = Quiz.find(4).question
-    end
-    
-    
-    def question_2_check
-        puts "answer submited"
-        redirect_to "/question_3"
+         @question2 = Quiz.find(2).question
     end
     
     
     def question_3
+         @question3 = Quiz.find(3).question
+    end
+    
+    
+    def question_4
+         @question4 = Quiz.find(4).question
+    end
+    
+    
+    def question_5
+         @question5 = Quiz.find(5).question
+    end
+    
+    
+    def question_6
+         @question6 = Quiz.find(6).question
+    end
+    
+    
+    def question_7
+         @question7 = Quiz.find(7).question
+    end
+    
+    
+    def question_8
+         @question8 = Quiz.find(8).question
+    end
+    
+    
+    def question_9
+         @question9 = Quiz.find(9).question
+    end
+    
         
+    def question_10
+         @question10 = Quiz.find(10).question
     end
 
     
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,26 @@ Rails.application.routes.draw do
 root 'quiz#start'    
     
     get '/question_1', to: 'quiz#question_1'
-    post '/question_1', to: 'quiz#question_1_check'
     
     get '/question_2', to: 'quiz#question_2'
-    post '/question_2', to: 'quiz#question_2_check'
     
     get '/question_3', to: 'quiz#question_3'
     
+    get '/question_4', to: 'quiz#question_4'
+    
+    get '/question_5', to: 'quiz#question_5'
+    
+    get '/question_6', to: 'quiz#question_6'
+    
+    get '/question_7', to: 'quiz#question_7'
+    
+    get '/question_8', to: 'quiz#question_8'
+    
+    get '/question_9', to: 'quiz#question_9'
+    
+    get '/question_10', to: 'quiz#question_10'
     
     
+    post '/question_check', to: 'quiz#question_check'
     
 end
