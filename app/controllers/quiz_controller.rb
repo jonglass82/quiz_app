f6c83a1886015e4037595f7ba6ccd3559274dbe7
Jon Glass <jonglass82@gmail.com>
2019-03-05 00:17:23 -0600

    complete score logic

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -10,24 +10,18 @@ class QuizController < ApplicationController
     def questions
         @question = Quiz.find_by_id(@@index).question
         @answers = Quiz.find_by_id(@@index).answers
-        @correct_answer = Quiz.find_by_id(@@index).answers.where("correct":true).select("answer")
     end
     
     
     def question_check
 
-        selected = params["selected"]
+        @selected = params["selected"]
 
-        puts selected
-        puts @correct_answer
-
-        if selected == @correct_answer
+        if @selected == "true"
             $score += 1
         end
 
         @@index += 1
-
-        puts $score
 
         redirect_to "/questions"
    
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,109 +6,76 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# quiz = Quiz.create("question": "Name the old bowling alley before Brunnswick (hint: it used to be in the jewel parking lot).", "answers": ["LZ Lanes", "King Pinz", "Town and Country Lanes", "Strike Zone"], "correct_answer": "Town and Country Lanes")
+# quiz = Quiz.create("question": " ", "answers": ["answer1", "answer2", "answer3", "answer4"], "correct_answer": " ")
 
-# quiz = Quiz.create("question": "Name the festival that takes place every summer in July.", "answers": ["Summer Days", "Alpine Fest", "Lion Fest", "None of the above"], "correct_answer": "Alpine Fest")
+# quiz = Quiz.create("question": " ", "answers": ["answer1", "answer2", "answer3", "answer4"], "correct_answer": " ")
 
-# quiz = Quiz.create("question": "Where did the Barn Dances take place?", "answers": ["Lions Club", "Middle School North", "Paulus Park", "Hackney's"], "correct_answer": "Paulus Park")
+# quiz = Quiz.create("question": " ", "answers": ["answer1", "answer2", "answer3", "answer4"], "correct_answer": " ")
 
-# quiz = Quiz.create("question": "What was the name of the old bar next door to JJ Twiggs (the one on the lake)", "answers": ["Rock and Roll Grill", "Jack's", "Lake Zurich Inn", "Frankie's"], "correct_answer": "Rock and Roll Grill")
+# quiz = Quiz.create("question": " ", "answers": ["answer1", "answer2", "answer3", "answer4"], "correct_answer": " ")
 
-# quiz = Quiz.create("question": "Name the restaurant that was there before Bobbers.", "answers": ["Eng's Tea House", "The Conservatory", "Good Time Charlie's", "Pete's Steakhouse"], "correct_answer": "Eng's Tea House")
+# quiz = Quiz.create("question": " ", "answers": ["answer1", "answer2", "answer3", "answer4"], "correct_answer": " ")
 
-# answer = Answer.create("answer": "Red Hots", "quiz_id":2, "correct":false )
+# quiz = Quiz.create("question": " ", "answers": ["answer1", "answer2", "answer3", "answer4"], "correct_answer": " ")
 
-# answer = Answer.create("answer": "Country Dog", "quiz_id":2, "correct":true )
+# quiz = Quiz.create("question": " ", "answers": ["answer1", "answer2", "answer3", "answer4"], "correct_answer": " ")
 
-# answer = Answer.create("answer": "Phil's", "quiz_id":2, "correct":false )
+# quiz = Quiz.create("question": " ", "answers": ["answer1", "answer2", "answer3", "answer4"], "correct_answer": " ")
 
-# answer = Answer.create("answer": "Main Dog", "quiz_id":2, "correct":false )
+# quiz = Quiz.create("question": " ", "answers": ["answer1", "answer2", "answer3", "answer4"], "correct_answer": " ")
 
+# quiz = Quiz.create("question": " ", "answers": ["answer1", "answer2", "answer3", "answer4"], "correct_answer": " ")
 
 
-# answer = Answer.create("answer": "Janzen's", "quiz_id":3, "correct":true )
 
-# answer = Answer.create("answer": "Paulus Park", "quiz_id":3, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":1, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":1, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":1, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":1, "correct":false )
 
-# answer = Answer.create("answer": "Sarah Adams", "quiz_id":3, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":2, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":2, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":2, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":2, "correct":false )
 
-# answer = Answer.create("answer": "Walgreens", "quiz_id":3, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":3, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":3, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":3, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":3, "correct":false )
 
+# answer = Answer.create("answer": " ", "quiz_id":4, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":4, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":4, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":4, "correct":false )
 
+# answer = Answer.create("answer": " ", "quiz_id":5, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":5, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":5, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":5, "correct":false )
 
+# answer = Answer.create("answer": " ", "quiz_id":6, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":6, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":6, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":6, "correct":false )
 
-# answer = Answer.create("answer": "Eagle", "quiz_id":4, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":7, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":7, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":7, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":7, "correct":false )
 
-# answer = Answer.create("answer": "Bennett Farms", "quiz_id":4, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":8, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":8, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":8, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":8, "correct":false )
 
-# answer = Answer.create("answer": "Country Orchard", "quiz_id":4, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":9, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":9, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":9, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":9, "correct":false )
 
-# answer = Answer.create("answer": "Bell's", "quiz_id":4, "correct":true )
+# answer = Answer.create("answer": " ", "quiz_id":10, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":10, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":10, "correct":false )
+# answer = Answer.create("answer": " ", "quiz_id":10, "correct":false )
 
-
-
-
-# answer = Answer.create("answer": "Gutterballz", "quiz_id":5, "correct":false )
-
-# answer = Answer.create("answer": "Country Lanes", "quiz_id":5, "correct":true )
-
-# answer = Answer.create("answer": "King Pin", "quiz_id":5, "correct":false )
-
-# answer = Answer.create("answer": "Shore Lanes", "quiz_id":5, "correct":false )
-
-
-
-
-# answer = Answer.create("answer": "LZ Days", "quiz_id":6, "correct":false )
-
-# answer = Answer.create("answer": "Summerfest", "quiz_id":6, "correct":false )
-
-# answer = Answer.create("answer": "Lion's Fest", "quiz_id":6, "correct":false )
-
-# answer = Answer.create("answer": "Alpine Fest", "quiz_id":6, "correct":true )
-
-
-
-
-# answer = Answer.create("answer": "Middle School North", "quiz_id":7, "correct":false )
-
-# answer = Answer.create("answer": "Middle School South", "quiz_id":7, "correct":false )
-
-# answer = Answer.create("answer": "Paulus Park", "quiz_id":7, "correct":true )
-
-# answer = Answer.create("answer": "Crestwood", "quiz_id":7, "correct":false )
-
-
-
-
-
-# answer = Answer.create("answer": "Rock and Roll Grill", "quiz_id":8, "correct":true )
-
-# answer = Answer.create("answer": "Blackie's", "quiz_id":8, "correct":false )
-
-# answer = Answer.create("answer": "Dipieros", "quiz_id":8, "correct":false )
-
-# answer = Answer.create("answer": "Giordiano's", "quiz_id":8, "correct":false )
-
-
-
-
-# answer = Answer.create("answer": "Eng's Tea House", "quiz_id":9, "correct":true )
-
-# answer = Answer.create("answer": "Kong Ming", "quiz_id":9, "correct":false )
-
-# answer = Answer.create("answer": "Tao's", "quiz_id":9, "correct":false )
-
-# answer = Answer.create("answer": "China King", "quiz_id":9, "correct":false )
-
-
-
-
-# answer = Answer.create("answer": "IHOP", "quiz_id":10, "correct":false )
-
-# answer = Answer.create("answer": "Koffee Kup", "quiz_id":10, "correct":true )
-
-# answer = Answer.create("answer": "Denny's", "quiz_id":10, "correct":false )
-
-# answer = Answer.create("answer": "Omega", "quiz_id":10, "correct":false )
 
